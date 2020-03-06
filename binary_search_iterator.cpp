a78ddb644d8b0c7b4ad26ff7ac31400942f5eb41
izaguirrejoe <izaguirrejoe@gmail.com>
2020-03-06 09:49:45 -0800

    Create binary_search_iterator.cpp

---
+++ binary_search_iterator.cpp
@@ -0,0 +1,18 @@
+template <class Iterator, class Element>
+Iterator my_binary_search (Iterator begin, Iterator end, const Element& key){
+    Iterator not_found = end;
+    while (begin <= end){
+        Iterator mid = begin + (end - begin)/2;
+        if (*mid == key)
+            return mid;
+        
+        else if (key > *mid){ //search the right half
+            begin = mid+1;
+        }
+        
+        else{ //search the left half
+            end = mid-1;
+        }
+    }
+    return not_found;
+}
